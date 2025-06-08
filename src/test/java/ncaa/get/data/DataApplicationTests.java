cf3689b34d73feb01dc48502f9d622974c4bb09e
Adam Maas <adamjm1220@gmail.com>
2025-06-08 15:06:07 -0400

    Initial Build

--- src/test/java/ncaa/get/data/DataApplicationTests.java
+++ src/test/java/ncaa/get/data/DataApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.TestPropertySource;
 @SpringBootTest
 @TestPropertySource(properties = {
     "data.localsavepoint=localsave/point",
+	"api.url=https://api.example.com/data",
 })
 class DataApplicationTests {
 
